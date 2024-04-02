e48d846c7378f8b028e55cabdea61464834a2a45
Sean Walsh <spwalsh01@gmail.com>
2024-04-02 17:18:38 -0400

    variables set

---
+++ modules/mongodb/s3.tf
@@ -0,0 +1,17 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  bucket = var.cluster_name
+  
+  control_object_ownership = true
+  object_ownership = "BucketOwnerPreferred"
+  block_public_acls = false
+  block_public_policy = false
+  restrict_public_buckets = false
+  ignore_public_acls = false
+  acl = "public-read"
+
+
+  versioning = {
+    enabled = true
+  }
+}
