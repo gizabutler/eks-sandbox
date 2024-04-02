2a9ee8fee6349272eb9b7c97043bf9e007feeb5b
Sean Walsh <spwalsh01@gmail.com>
2024-04-02 17:37:10 -0400

    variables updated

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -2,3 +2,14 @@ variable "cluster_name" {
   description = "cluster name"
   type        = string
 }
+
+variable "vpc_id" {
+    description = "VPC Id"
+    type = string
+}
+
+
+variable "private_subnets" {
+    description = "Subnet IDs"
+    type = list
+}
--- variables.tf
+++ variables.tf
@@ -10,3 +10,16 @@ variable "cluster_name" {
   description = "cluster name"
   type        = string
   default     = "test"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+  default     = "test"
+}
+
+variable "private_subnets" {
+  description = "Subnet IDs"
+  type        = string
+  default     = "test"
+}
